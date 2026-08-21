31af989e8dd65cf0dcc37741334b03e5f69545f1
ChatApp Messenger <shahronmahmadmurodzoda08@gmail.com>
2026-08-21 14:42:36 +0500

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,14 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
-/// ЭЗОҲ: Ҳозир ҳамаи платформаҳо (Android, iOS, Web) як танзими ягонаро
-/// истифода мебаранд — калидҳои проекти Firebase (chatapp-57fb2). Ин кор
-/// мекунад, чунки Firebase бо нобаёт калидро ба платформа қулф намекунад
-/// (агар шумо худ дар Google Cloud Console маҳдудият нагузошта бошед).
-/// Дар оянда, агар хоҳед хусусиятҳои махсуси Android (push notifications,
-/// Crashlytics ва ғ.) илова кунед, метавонед апп-и алоҳидаи Android дар
-/// Firebase Console сабт карда, ин файлро бо калидҳои он иваз кунед.
 class DefaultFirebaseOptions {
-  static FirebaseOptions get currentPlatform => web;
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions барои ин платформа танзим нашудааст. '
+          'Танҳо Web ва Android дастгирӣ мешаванд.',
+        );
+    }
+  }
 
   static const FirebaseOptions web = FirebaseOptions(
     apiKey: 'AIzaSyDO8rw1NvSAosX7Z0Nj4_eV1hkBAB6OW1A',
@@ -18,5 +26,13 @@ class DefaultFirebaseOptions {
     messagingSenderId: '712168365642',
     appId: '1:712168365642:web:6ea9ac370500cc8b6310b8',
     measurementId: 'G-D78QHLSTVN',
+  );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyAi83BIGIYOibG2YcwtYY1Lah9ePkPtLWQ',
+    appId: '1:712168365642:android:3bb79f163e73fe9f6310b3',
+    messagingSenderId: '712168365642',
+    projectId: 'chatapp-57fb2',
+    storageBucket: 'chatapp-57fb2.firebasestorage.app',
   );
 }
