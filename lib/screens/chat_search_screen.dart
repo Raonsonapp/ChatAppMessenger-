d5aa094f400d6f7d19937a0efbae155647bb8777
ChatApp Messenger <shahronmahmadmurodzoda08@gmail.com>
2026-08-26 22:08:38 +0500

    Update chat_search_screen.dart

--- lib/screens/chat_search_screen.dart
+++ lib/screens/chat_search_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+Import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
